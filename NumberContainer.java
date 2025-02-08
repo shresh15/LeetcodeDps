3e1c7021c88bd1e641251403e82fdc3d98f76af0
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-08 23:12:11 +0530

    Number container System

---
+++ NumberContainer.java
@@ -0,0 +1,51 @@
+/*
+Design a Number Container System
+
+Design a number container system that can do the following:
+
+Insert or Replace a number at the given index in the system.
+Return the smallest index for the given number in the system.
+Implement the NumberContainers class:
+
+NumberContainers() Initializes the number container system.
+void change(int index, int number) Fills the container at index with the number. If there is already a number at that index, replace it.
+int find(int number) Returns the smallest index for the given number, or -1 if there is no index that is filled by number in the system.
+*/
+class NumberContainers {
+    private Map<Integer, Integer> indexToNumber;
+    private Map<Integer, TreeSet<Integer>> numberToIndices;
+
+    public NumberContainers() {
+        indexToNumber = new HashMap<>();
+        numberToIndices = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        if (indexToNumber.containsKey(index)) {
+            int oldNumber = indexToNumber.get(index);
+            if (oldNumber != number) {
+                numberToIndices.get(oldNumber).remove(index);
+                if (numberToIndices.get(oldNumber).isEmpty()) {
+                    numberToIndices.remove(oldNumber);
+                }
+            }
+        }
+        
+        indexToNumber.put(index, number);
+        numberToIndices.computeIfAbsent(number, k -> new TreeSet<>()).add(index);
+    }
+
+    public int find(int number) {
+        if (!numberToIndices.containsKey(number) || numberToIndices.get(number).isEmpty()) {
+            return -1;
+        }
+        return numberToIndices.get(number).first();
+    }
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
